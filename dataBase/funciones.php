ae2a9ed7ffd6ced7af98139ed3f3fdfe06f900e4
Jonathan <jonathanvel84@gmail.com>
2022-05-07 21:39:12 -0500

    Cambiadas las funciones de Mysqli a PDO

--- dataBase/funciones.php
+++ dataBase/funciones.php
@@ -2,29 +2,14 @@
 
 function getUserName()
 {
-    /*
-    require 'conexiondb.php';
-
-    $sql = "SELECT nombre, apellidos FROM cliente WHERE id = '" . $_SESSION["id"] . "' ";
-    $ejecutar = mysqli_query($conexion, $sql);
-
-    $fila = mysqli_fetch_assoc($ejecutar);
-    //Convertir el Array a un String
-    $filaString = implode(' ', $fila);
-
-    return $filaString;
-
-    $ejecutar->free();
-    $conexion->close();
-    */
 
     require 'conexiondbPDO.php';
 
-    $query = "SELECT nombre, apellidos 
+    $sql = "SELECT nombre, apellidos 
               FROM cliente 
               WHERE id= '" . $_SESSION["id"] . "'  ";
 
-    $ejecutar = $conexion->prepare($query);
+    $ejecutar = $conexion->prepare($sql);
 
     $ejecutar->execute();
 
@@ -39,106 +24,107 @@ function getUserName()
 function getSolicitudes()
 {
 
-    require 'conexiondb.php';
+    require 'conexiondbPDO.php';
 
-    $sql1 = "SELECT * FROM solicitudes 
+    $sql = "SELECT * FROM solicitudes 
               INNER JOIN cliente ON cliente.id = solicitudes.cliente_id
               WHERE '" . $_SESSION["id"] . "' = solicitudes.cliente_id ";
 
-    $ejecutar1 = mysqli_query($conexion, $sql1);
+    $ejecutar = $conexion->prepare($sql);
+    $ejecutar->execute();
 
+    if ($ejecutar->rowCount() > 0) {
 
-    if ($ejecutar1->num_rows > 0) {
-        while ($filas1 = $ejecutar1->fetch_assoc()) {
+        while ($filas = $ejecutar->fetch(PDO::FETCH_OBJ)) {
 
 ?>
-
             <tr>
-                <td><?php echo $filas1["fecha_solicitud"]; ?></td>
-                <td><?php echo $filas1["estado"]; ?></td>
-                <td><?php echo $filas1["detalles_servicio"]; ?></td>
+                <td><?php echo $filas->fecha_solicitud; ?></td>
+                <td><?php echo $filas->estado; ?></td>
+                <td><?php echo $filas->detalles_servicio; ?></td>
                 <td><a href="#"><i class="gg-search"></i></a></td>
             </tr>
         <?php
         }
     } else {
-        echo '<tr><td>Aún no tiene Solicitudes </td> </tr>';
+        echo "<tr><td>Aún no tiene Servicios</td></tr>";
     }
 
-    $ejecutar1->free();
-    $conexion->close();
+    $conexion = null;
 }
 
 function getDatosBasicosCuenta()
 {
-    require 'conexiondb.php';
+    require 'conexiondbPDO.php';
 
     if (isset($_SESSION['id'])) {
 
-        $sql2 = "SELECT * FROM cliente 
+        $sql = "SELECT * FROM cliente 
                WHERE id = '" . $_SESSION["id"] . "' ";
-        $ejecutar2 = mysqli_query($conexion, $sql2);
+        $ejecutar = $conexion->prepare($sql);
+        $ejecutar->execute();
 
-        $filas2 = mysqli_fetch_array($ejecutar2);
+        $filas = $ejecutar->fetch(PDO::FETCH_OBJ);
 
         ?>
-        <li>Tipo Documento: <p><?php echo $filas2["tipo_documento"]; ?></p>
+        <li>Tipo Documento: <p><?php echo $filas->tipo_documento; ?></p>
         </li>
-        <li>Número Documento: <p><?php echo $filas2["numero_documento"]; ?></p>
+        <li>Número Documento: <p><?php echo $filas->numero_documento; ?></p>
         </li>
-        <li>Nombre: <p><?php echo $filas2["nombre"]; ?></p>
+        <li>Nombre: <p><?php echo $filas->nombre; ?></p>
         </li>
-        <li>Apellidos: <p><?php echo $filas2["apellidos"]; ?></p>
+        <li>Apellidos: <p><?php echo $filas->apellidos; ?></p>
         </li>
-        <li>Email: <p><?php echo $filas2["email"]; ?></p>
+        <li>Email: <p><?php echo $filas->email; ?></p>
         </li>
-        <li>Celular: <p><?php echo $filas2["numero_celular"]; ?></p>
+        <li>Celular: <p><?php echo $filas->numero_celular; ?></p>
         </li>
-        <li>Dirección: <p><?php echo $filas2["direccion"]; ?></p>
+        <li>Dirección: <p><?php echo $filas->direccion; ?></p>
         </li>
         </ul>
         <a id="btn-modal" href="#">Editar</a>
     <?php
     }
-    $ejecutar2->free();
-    $conexion->close();
+
+    $conexion = null;
 }
 
 function getDatosHogarCuenta()
 {
-    require 'conexiondb.php';
+    require 'conexiondbPDO.php';
 
-    $sql3 = "SELECT * FROM cliente_hogar 
+    $sql = "SELECT * FROM cliente_hogar 
              INNER JOIN cliente 
              WHERE '" . $_SESSION["id"] . "' = cliente_hogar.cliente_id ";
 
-    $ejecutar3 = mysqli_query($conexion, $sql3);
+    $ejecutar = $conexion->prepare($sql);
+    $ejecutar->execute();
 
-    if ($ejecutar3->num_rows > 0) {
-        $filas3 = $ejecutar3->fetch_assoc();
+    if ($ejecutar->rowCount() > 0) {
+        $filas = $ejecutar->fetch(PDO::FETCH_OBJ);
 
 
     ?>
         <ul>
             <li>
                 <img src="img/iconos/hogar.png" alt="">
-                <p>Mt2: <?php echo $filas3["hogar_en_mt2"]; ?></p>
+                <p>Mt2: <?php echo $filas->hogar_en_mt2; ?></p>
             </li>
             <li>
                 <img src="img/iconos/habitacion.png" alt="">
-                <p>N° Habitaciones: <?php echo $filas3["numero_habitaciones"]; ?></p>
+                <p>N° Habitaciones: <?php echo $filas->numero_habitaciones; ?></p>
             </li>
             <li>
                 <img src="img/iconos/baño.png" alt="">
-                <p>N° Baños: <?php echo $filas3["numero_bathing"]; ?></p>
+                <p>N° Baños: <?php echo $filas->numero_bathing; ?></p>
             </li>
             <li>
                 <img src="img/iconos/personas.png" alt="">
-                <p>N° Personas: <?php echo $filas3["numero_personas"]; ?></p>
+                <p>N° Personas: <?php echo $filas->numero_personas; ?></p>
             </li>
             <li>
                 <img src="img/iconos/mascotas.png" alt="">
-                <p>Mascotas: <?php echo $filas3["mascotas"]; ?></p>
+                <p>Mascotas: <?php echo $filas->mascotas; ?></p>
             </li>
         </ul>
 
@@ -146,8 +132,7 @@ function getDatosHogarCuenta()
     } else {
         echo '<p id="else-while">Sin datos, por favor de click en el Botón de "Editar" para agregar la información de su Hogar</p>';
     }
-    $ejecutar3->free();
-    $conexion->close();
+    $conexion = null;
     ?>
     <a id="btn-modal" href="#">Editar</a>
 <?php
